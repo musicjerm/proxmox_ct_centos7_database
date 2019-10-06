204cd6d2448e50abf447970819e45448460c6a85
musicjerm <musicjerm@users.noreply.github.com>
2019-10-05 22:37:29 -0600

    update to mdb 10.4

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@
 yum update -y
 
 # add repo
-echo "# MariaDB 10.2 CentOS repository list" > /etc/yum.repos.d/mariadb.repo
+echo "# MariaDB 10.4 CentOS repository list" > /etc/yum.repos.d/mariadb.repo
 echo "# http://downloads.mariadb.org/mariadb/repositories/" >> /etc/yum.repos.d/mariadb.repo
 echo "[mariadb]" >> /etc/yum.repos.d/mariadb.repo
 echo "name = MariaDB" >> /etc/yum.repos.d/mariadb.repo
-echo "baseurl = http://yum.mariadb.org/10.2/centos7-amd64" >> /etc/yum.repos.d/mariadb.repo
+echo "baseurl = http://yum.mariadb.org/10.4/centos7-amd64" >> /etc/yum.repos.d/mariadb.repo
 echo "gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB" >> /etc/yum.repos.d/mariadb.repo
 echo "gpgcheck=1" >> /etc/yum.repos.d/mariadb.repo
 
